e6a47a670bacabc646566036cb0af139aa1b2b33
Pius Alfred <me.pius1102@gmail.com>
2021-10-24 01:03:00 +0300

    errors

--- errors/errors.go
+++ errors/errors.go
@@ -25,6 +25,22 @@
 
 package errors
 
+var _ error = (*Code)(nil)
+
+type (
+	Code struct {
+		ID string
+		Desc string
+		Err error
+	}
+)
+
+func (c *Code) Error() string {
+	return c.Desc
+}
+
+func (e *Code) Unwrap() error { return e.Err }
+
 const (
 	ESB000001 = "Something went wrong"
 	ESB000004 = "An error occurred while initiating the payment"
@@ -37,3 +53,19 @@ const (
 	ESB000036 = "Invalid MSISDN Length. MSISDN Length should be {0} and should start with 0"
 	ESB000039 = "Vendor is not configured to do transaction in the country {0}"
 )
+
+var (
+	Codes = map[string]string{
+		"ESB000001":ESB000001,
+		"ESB000004":ESB000004,
+		"ESB000008":ESB000008,
+		"ESB000011":ESB000011,
+		"ESB000014":ESB000014,
+		"ESB000033":ESB000033,
+		"ESB000034":ESB000034,
+		"ESB000035":ESB000035,
+		"ESB000036":ESB000036,
+		"ESB000039":ESB000039,
+	}
+)
+
